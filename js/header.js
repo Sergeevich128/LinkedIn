94c2eeebc51c5c99e233865d71b2368ed8ecd165
Sergeevich128 <mironpichkurenko@gmail.com>
2020-07-06 17:07:41 +0300

    headern adaptation 7.0

--- js/header.js
+++ js/header.js
@@ -1,44 +1,24 @@
 const header = document.getElementsByTagName('header')[0];
-const userData = document.getElementsByClassName('user-data')[0];
-const userInfo = document.getElementsByClassName('user-information')[0];
-const searchContainer = document.getElementsByClassName('search-container')[0];
-const menuBtn = document.getElementsByClassName('menu-btn')[0];
-const navigation = document.getElementsByClassName('navigation')[0];
-const profileMore = document.getElementsByClassName('profile-more')[0];
 
-header.addEventListener('click', (event) => {
-    if (event.target === searchContainer) {
-        searchContainer.classList.add('active');
-        userData.classList.remove('active');
+document.addEventListener('click', (event) => {
+    const elem = event.target;
+    if (elem.closest('.menu-btn')) {
+        header.classList.toggle('active-btn');
+    } else if (elem.closest('.profile-more')) {
+        header.classList.toggle('active-profile');
+        return;
+    } else if (elem.closest('.other')) {
+        header.classList.toggle('active-other');
+    } else if (elem.closest('.icon-menu')) {
+        header.classList.toggle('active-menu');
+    } else if (elem.closest('.icon-other')) {
+        header.classList.toggle('active-other');
     }
 
-    if (event.target === userData) {
-        userData.classList.add('active');
-        searchContainer.classList.remove('active');
-    }
-
-    if (event.target === menuBtn) {
-        if (menuBtn.classList.contains('active')) {
-            menuBtn.classList.remove('active');
-            navigation.classList.remove('active');
-        } else {
-            menuBtn.classList.add('active');
-            navigation.classList.add('active');
-            profileMore.classList.remove('active');
-            userInfo.classList.remove('active');
-        }
-    }
-
-    if (event.target === profileMore) {
-        if (profileMore.classList.contains('active')) {
-            profileMore.classList.remove('active');
-            userInfo.classList.remove('active');
-        } else {
-            profileMore.classList.add('active');
-            userInfo.classList.add('active');
-            menuBtn.classList.remove('active');
-            navigation.classList.remove('active');
-        }
+    if (elem.closest('.search-container')) {
+        header.className = 'active-search';
+    } else if (elem.closest('.user-data')) {
+        header.className = '';
     }
 });
 

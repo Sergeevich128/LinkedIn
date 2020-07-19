097c31df7a6249e9d23152acfe4509191359fb82
Sergeevich128 <mironpichkurenko@gmail.com>
2020-07-19 22:08:34 +0300

    header adaptation 10.0

--- js/header.js
+++ js/header.js
@@ -1,21 +1,26 @@
 const header = document.getElementsByTagName('header')[0];
+const body = document.getElementsByTagName('body')[0];
 
 document.addEventListener('click', (event) => {
-    const elem = event.target;
-    header.classList.remove('active-btn', 'active-profile', 'active-other');
-    if (elem.closest('.menu-btn')) {
-        header.classList.toggle('active-btn');
-        console.log('click')
-    } else if (elem.closest('.profile-more')) {
-        header.classList.toggle('active-profile');
-        return;
-    } else if (elem.closest('.other')) {
-        header.classList.toggle('active-other');
+    const parent = event.target.closest('.menu-js');
+    if (parent) {
+        if (parent.dataset.menu === body.dataset.menu) {
+            body.setAttribute('data-menu', '');
+            console.log(parent)
+        } 
+        else {
+            body.setAttribute('data-menu', parent.dataset.menu);
+            console.log(parent)
+        }
+    } 
+    else {
+        body.setAttribute('data-menu', '');
     }
+});
 
-    if (elem.closest('.search-container')) {
-        header.className = 'active-search';
-    } else if (elem.closest('.user-data')) {
-        header.className = '';
-    }
+document.addEventListener('click', (event) => {
+    const parent = event.target.closest('.menu-switch');
+    if (parent) {
+            header.setAttribute('data-menu', parent.dataset.menu);
+    } 
 });

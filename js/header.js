6d4f42fada5aae765e33b66e4333e7098f9d7b89
Sergeevich128 <mironpichkurenko@gmail.com>
2020-07-23 19:29:45 +0300

    header adaptation 14.0

--- js/header.js
+++ js/header.js
@@ -1,8 +1,6 @@
 const header = document.getElementsByTagName('header')[0];
 const body = document.getElementsByTagName('body')[0];
 const searchContainer = document.getElementsByClassName('search-container')[0];
-const userData = document.getElementsByClassName('user-data')[0];
-const menuBurger = document.getElementsByClassName('menu-burger')[0];
 
 document.addEventListener('click', (event) => {
     const parent = event.target.closest('.menu-js');
@@ -11,6 +9,10 @@ document.addEventListener('click', (event) => {
     if (parent) {
         menuName = parent.dataset.menu;
     }
+    if (parent === searchContainer) {
+        return false;
+    }
+
     body.setAttribute(
         'data-menu',
         body.dataset.menu === menuName ? '' : menuName
@@ -21,9 +23,4 @@ header.addEventListener('click', (event) => {
     event.target.closest('.search-container') ?
         header.className = 'active-search' :
         header.className = 'active-data';
-    // if (event.target.closest('.search-container')) {
-    //     header.className = 'active-search';
-    //     return;
-    // }
-    // header.className = 'active-data';
 });

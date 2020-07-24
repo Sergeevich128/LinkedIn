453ecfbab083e3fd7ba784e4c81089e8f7e75891
Sergeevich128 <mironpichkurenko@gmail.com>
2020-07-24 12:00:01 +0300

    header adaptation 16.0

--- js/header.js
+++ js/header.js
@@ -1,7 +1,6 @@
 const header = document.getElementsByTagName('header')[0];
 const body = document.getElementsByTagName('body')[0];
 const searchContainer = document.getElementsByClassName('search-container')[0];
-const profileMore = document.getElementsByClassName('profile-more')[0];
 
 document.addEventListener('click', (event) => {
     const parent = event.target.closest('.menu-js');
@@ -10,13 +9,14 @@ document.addEventListener('click', (event) => {
     if (parent) {
         menuName = parent.dataset.menu;
     }
-    if (parent === searchContainer) return;
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        if (parent === searchContainer) return;
+    }
 
     body.setAttribute(
         'data-menu',
         body.dataset.menu === menuName ? '' : menuName
     );
-
 });
 
 header.addEventListener('click', (event) => {
@@ -25,6 +25,4 @@ header.addEventListener('click', (event) => {
     } else if (event.target.closest('.user-data')) {
         header.className = 'active-user';
     }
-    //event.target.closest('.search-container') ? header.className = 'active-search' :
-    //event.target.closest('.user-data') ? header.className = 'active-user';
 });
